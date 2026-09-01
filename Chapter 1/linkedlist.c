a950d04d2ebdce51dc0f25ba43baacbdb20d96e2
bagherim8650 <mbagheri@cpp.edu>
2026-09-01 13:45:26 -0700

    updated linkedlist

--- Chapter 1/linkedlist.c
+++ Chapter 1/linkedlist.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    printf("hello world");
+    printf("Creating new linked list 'pets'");
+    struct linkedList pets;
+
 }
 
-typedef struct{
-    char contents[];
-    Node prev;
-    Node next;
-} Node;
+struct linkedList
+{
+    struct Node head;
+};
+
+
+struct Node{
+    void *data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+struct Node* insertAfter(struct Node *target, void *data){
+    //allocate memory for node
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = malloc(sizeof(*data));
+    newNode->next = target->next;
+    newNode->prev = target;
+
+    //copy new node to memory allocated
+    memcpy(newNode->data, data, sizeof(*data));
+};
