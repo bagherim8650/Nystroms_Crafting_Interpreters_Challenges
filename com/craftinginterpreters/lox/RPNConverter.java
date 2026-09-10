458bc6fa342f28b2f85509b11e05dca5a5208cb8
bagherim8650 <mbagheri@cpp.edu>
2026-09-10 16:21:57 -0700

    used visitor to create Reverse Polish Notation converter for expressions

--- com/craftinginterpreters/lox/RPNConverter.java
+++ com/craftinginterpreters/lox/RPNConverter.java
@@ -7,24 +7,78 @@ import com.craftinginterpreters.lox.Expr.Unary;
 
 public class RPNConverter implements Expr.Visitor<String>{
 
+    public String print(Expr expr){
+        return expr.accept(this);
+    }
+
+    public String convert(Expr expr){
+        return expr.accept(this);
+    }
+
     @Override
     public String visitBinaryExpr(Binary expr) {
-        return null;
+        
+        return print(expr.left) + ' ' + print(expr.right) + expr.operator.lexeme;
     }
 
     @Override
     public String visitGroupingExpr(Grouping expr) {
-        return null;
+        return print(expr.expression);
     }
 
     @Override
     public String visitLiteralExpr(Literal expr) {
-        return null;
+        if (expr.value == null) {
+            return "nil";
+        } else {
+            return expr.value.toString();
+        }
     }
 
     @Override
     public String visitUnaryExpr(Unary expr) {
-        return null;
+        return print(expr.right) + ' ' + expr.operator.lexeme;
     }
-    
+
+    public static void main(String[] args) {
+        RPNConverter converter = new RPNConverter();
+        Token plus  = new Token(TokenType.PLUS,  "+", null, 1);
+        Token minus = new Token(TokenType.MINUS, "-", null, 1);
+        Token star  = new Token(TokenType.STAR,  "*", null, 1);
+
+        // Test 1: 1
+        Expr e1 = new Expr.Literal(1);
+        System.out.println("1 -> " + converter.convert(e1));
+
+        // Test 2: 1 + 2
+        Expr e2 = new Expr.Binary(
+            new Expr.Literal(1),
+            plus,
+            new Expr.Literal(2));
+        System.out.println("1 + 2 -> " + converter.convert(e2));
+
+        // Test 3: -1
+        Expr e3 = new Expr.Unary(minus, new Expr.Literal(1));
+        System.out.println("-1 -> " + converter.convert(e3));
+
+        // Test 4: (1 + 2) * (4 - 3)
+        Expr e4 = new Expr.Binary(
+            new Expr.Grouping(new Expr.Binary(
+                new Expr.Literal(1), plus, new Expr.Literal(2))),
+            star,
+            new Expr.Grouping(new Expr.Binary(
+                new Expr.Literal(4), minus, new Expr.Literal(3))));
+        System.out.println("(1 + 2) * (4 - 3) -> " + converter.convert(e4));
+
+        // Test 5: 1 + 2 * 3 - 4
+        Expr e5 = new Expr.Binary(
+            new Expr.Binary(
+                new Expr.Literal(1),
+                plus,
+                new Expr.Binary(
+                    new Expr.Literal(2), star, new Expr.Literal(3))),
+            minus,
+            new Expr.Literal(4));
+        System.out.println("1 + 2 * 3 - 4 -> " + converter.convert(e5));
+    }
 }
