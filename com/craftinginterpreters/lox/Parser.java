ad33a557bc57f5788c8dbd488670ec74378fdd8a
bagherim8650 <mbagheri@cpp.edu>
2026-09-16 15:46:11 -0700

    added chapter 7 code

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -116,6 +116,32 @@ class Parser {
 			return new Expr.Grouping(expr);
 		}
 
+		//error cases (challenge 3, ch 6)
+
+		if(match(BANG_EQUAL, EQUAL_EQUAL)){
+			error(previous(), "Missing left-hand operand");
+			equality();
+			return null;
+		}
+
+		if(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)){
+			error(previous(), "Missing left-hand operand");
+			comparison();
+			return null;
+		}
+
+		if(match(PLUS)){
+			error(previous(), "Missing left-hand operand");
+			term();
+			return null;
+		}
+
+		if (match(SLASH, STAR)){
+			error(previous(), "Missing left-hand operand");
+			factor();
+			return null;
+		}
+
 		throw error(peek(), "Expect expression.");
 	}
 
