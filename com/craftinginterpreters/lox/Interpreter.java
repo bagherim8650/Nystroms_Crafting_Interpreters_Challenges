a19a3ebc57c45b75f9a57f5b0723c5984653a61c
bagherim8650 <mbagheri@cpp.edu>
2026-09-16 16:03:48 -0700

    added string concatenation

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -42,12 +42,11 @@ class Interpreter implements Expr.Visitor<Object> {
         checkNumberOperands(expr.operator, left, right);
 				return (double)left - (double)right;
       case PLUS:
-        if (left instanceof Double && right instanceof Double) {
+				if(left instanceof String || right instanceof String){
+					return stringify(left) + stringify(right);
+				}
+				if (left instanceof Double && right instanceof Double) {
           return (double)left + (double)right;
-        }
-
-        if (left instanceof String && right instanceof String) {
-          return (String)left + (String)right;
         }
         throw new RuntimeError(expr.operator,
             "Operands must be two numbers or two strings.");
