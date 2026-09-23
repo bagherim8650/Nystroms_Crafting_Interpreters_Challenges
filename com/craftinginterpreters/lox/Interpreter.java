8b436afd25d72bfafd51409e2f347960e5387955
bagherim8650 <mbagheri@cpp.edu>
2026-09-23 15:33:35 -0700

    completed chapter 8 additions

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -5,19 +5,19 @@ import java.util.List;
 import com.craftinginterpreters.lox.Expr.Ternary;
 
 class Interpreter implements Expr.Visitor<Object>,
-                              Stmt.Visitor<Void> {
+															Stmt.Visitor<Void> {
 
-  private Environment environment = new Environment();
+	private Environment environment = new Environment();
 
-  void interpret(List<Stmt> statements) {
-    try {
-      for (Stmt statement : statements) {
-        execute(statement);
-      }
-    } catch (RuntimeError error) {
-      Lox.runtimeError(error);
-    }
-  }
+	void interpret(List<Stmt> statements) {
+		try {
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
+		} catch (RuntimeError error) {
+			Lox.runtimeError(error);
+		}
+	}
 
 	@Override
 	public Object visitLiteralExpr(Expr.Literal expr) {
@@ -33,32 +33,59 @@ class Interpreter implements Expr.Visitor<Object>,
 		return expr.accept(this);
 	}
 
-  private void execute(Stmt stmt) {
-    stmt.accept(this);
-  }
+	private void execute(Stmt stmt) {
+		stmt.accept(this);
+	}
 
-  @Override
-  public Void visitExpressionStmt(Stmt.Expression stmt) {
-    evaluate(stmt.expression);
-    return null;
-  }
+  void executeBlock(List<Stmt> statements,
+                    Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
 
-  @Override
-  public Void visitPrintStmt(Stmt.Print stmt) {
-    Object value = evaluate(stmt.expression);
-    System.out.println(stringify(value));
-    return null;
-  }
-
-  @Override
-  public Void visitVarStmt(Stmt.Var stmt) {
-    Object value = null;
-    if (stmt.initializer != null) {
-      value = evaluate(stmt.initializer);
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
     }
+  }
 
-    environment.define(stmt.name.lexeme, value);
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
     return null;
+  }
+
+	@Override
+	public Void visitExpressionStmt(Stmt.Expression stmt) {
+		evaluate(stmt.expression);
+		return null;
+	}
+
+	@Override
+	public Void visitPrintStmt(Stmt.Print stmt) {
+		Object value = evaluate(stmt.expression);
+		System.out.println(stringify(value));
+		return null;
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		Object value = null;
+		if (stmt.initializer != null) {
+			value = evaluate(stmt.initializer);
+		}
+
+		environment.define(stmt.name.lexeme, value);
+		return null;
+	}
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
   }
 
 	@Override
@@ -130,10 +157,10 @@ class Interpreter implements Expr.Visitor<Object>,
 		return null;
 	}
 
-  @Override
-  public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
-  }
+	@Override
+	public Object visitVariableExpr(Expr.Variable expr) {
+		return environment.get(expr.name);
+	}
 
 
 private void checkNumberOperand(Token operator, Object operand) {
--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -16,48 +16,77 @@ class Parser {
 	}
 
 	private Expr expression() {
-		return separator();
+    return assignment();
 	}
 
-  private Stmt declaration() {
-    try {
-      if (match(VAR)) return varDeclaration();
+	private Stmt declaration() {
+		try {
+			if (match(VAR)) return varDeclaration();
 
-      return statement();
-    } catch (ParseError error) {
-      synchronize();
-      return null;
+			return statement();
+		} catch (ParseError error) {
+			synchronize();
+			return null;
+		}
+	}
+
+	private Stmt statement() {
+		if (match(PRINT)) return printStatement();
+    if (match(LEFT_BRACE)) return new Stmt.Block(block());
+		return expressionStatement();
+	}
+
+	private Stmt printStatement() {
+		Expr value = expression();
+		consume(SEMICOLON, "Expect ';' after value.");
+		return new Stmt.Print(value);
+	}
+
+	private Stmt varDeclaration() {
+		Token name = consume(IDENTIFIER, "Expect variable name.");
+
+		Expr initializer = null;
+		if (match(EQUAL)) {
+			initializer = expression();
+		}
+
+		consume(SEMICOLON, "Expect ';' after variable declaration.");
+		return new Stmt.Var(name, initializer);
+	}
+
+	private Stmt expressionStatement() {
+		Expr expr = expression();
+		consume(SEMICOLON, "Expect ';' after expression.");
+		return new Stmt.Expression(expr);
+	}
+
+  private List<Stmt> block() {
+    List<Stmt> statements = new ArrayList<>();
+
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      statements.add(declaration());
     }
+
+    consume(RIGHT_BRACE, "Expect '}' after block.");
+    return statements;
   }
 
-  private Stmt statement() {
-    if (match(PRINT)) return printStatement();
+  private Expr assignment() {
+    Expr expr = equality();
 
-    return expressionStatement();
-  }
-
-  private Stmt printStatement() {
-    Expr value = expression();
-    consume(SEMICOLON, "Expect ';' after value.");
-    return new Stmt.Print(value);
-  }
-
-  private Stmt varDeclaration() {
-    Token name = consume(IDENTIFIER, "Expect variable name.");
-
-    Expr initializer = null;
     if (match(EQUAL)) {
-      initializer = expression();
+      Token equals = previous();
+      Expr value = assignment();
+
+      if (expr instanceof Expr.Variable) {
+        Token name = ((Expr.Variable)expr).name;
+        return new Expr.Assign(name, value);
+      }
+
+      error(equals, "Invalid assignment target."); 
     }
 
-    consume(SEMICOLON, "Expect ';' after variable declaration.");
-    return new Stmt.Var(name, initializer);
-  }
-
-  private Stmt expressionStatement() {
-    Expr expr = expression();
-    consume(SEMICOLON, "Expect ';' after expression.");
-    return new Stmt.Expression(expr);
+    return expr;
   }
 
 	private Expr ternary(){
@@ -152,9 +181,9 @@ class Parser {
 			return new Expr.Literal(previous().literal);
 		}
 
-    if (match(IDENTIFIER)) {
-      return new Expr.Variable(previous());
-    }
+		if (match(IDENTIFIER)) {
+			return new Expr.Variable(previous());
+		}
 
 		if (match(LEFT_PAREN)) {
 			Expr expr = expression();
@@ -191,14 +220,14 @@ class Parser {
 		throw error(peek(), "Expect expression.");
 	}
 
-  List<Stmt> parse() {
-    List<Stmt> statements = new ArrayList<>();
-    while (!isAtEnd()) {
-      statements.add(declaration());
-    }
+	List<Stmt> parse() {
+		List<Stmt> statements = new ArrayList<>();
+		while (!isAtEnd()) {
+			statements.add(declaration());
+		}
 
-    return statements;
-  }
+		return statements;
+	}
 
 	private boolean match(TokenType... types) {
 		for (TokenType type : types) {
